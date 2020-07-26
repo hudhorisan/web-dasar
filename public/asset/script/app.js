c79183791127b2ebc86e69d681548d8d75ca07f6
hudhori <hudhorisan@gmail.com>
2020-07-26 17:09:25 +0700

    sidebar fixed

--- public/asset/script/app.js
+++ public/asset/script/app.js
@@ -1,10 +1,6 @@
 const links = document.querySelectorAll("a");
 
-for (const link of links) {
-    link.addEventListener("click", clickHandler);
-}
-
-function clickHandler(e) {
+function smoothScrool(e) {
     e.preventDefault();
     const href = this.getAttribute("href");
     const offsetTop = document.querySelector(href).offsetTop;
@@ -14,3 +10,19 @@ function clickHandler(e) {
         behavior: "smooth"
     });
 }
+
+for (const link of links) {
+    link.addEventListener("click", smoothScrool);
+}
+
+const side = document.getElementsByTagName('aside')[0];
+
+function stikySide() {
+
+    const offsetTop = side.offsetTop;
+    if (offsetTop < 30) {
+        side.classList.toggle('tempel')
+    }
+}
+
+side.addEventListener("scroll", stikySide);
